3a8050311017b8dfef63360278f6ff960279a33b
Pranav Aher <doingdifferent29@gmail.com>
2024-06-26 18:45:35 +0530

    Remove registration for admin
    Add admin controller with index action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, skip: [:registrations]
   devise_for :users
   resources :courses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
+  get "admin" => "admin#index"
 
   # Defines the root path route ("/")
   root "courses#index"
