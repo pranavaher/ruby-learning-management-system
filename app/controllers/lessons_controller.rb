f281bfc2c96b6ac0df70cc492af7d82ad7cf8494
Pranav Aher <doingdifferent29@gmail.com>
2024-07-02 17:04:16 +0530

    Setup Mark as complete button and its functionality in update action of lessons controller
    Exposed @completed_lessons to show action for rendering icons if lesson is completed

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,18 +1,33 @@
 class LessonsController < ApplicationController
   before_action :set_lesson, only: %i[ show update ]
+  before_action :set_course, only: %i[ show update ]
 
   # GET /lessons/1 or /lessons/1.json
   def show
+    @completed_lessons = current_user.lesson_users.where(completed: true).pluck(:lesson_id)
     @course = @lesson.course
   end
 
   # PATCH/PUT /lessons/1 or /lessons/1.json
   def update
+    @lesson_user = LessonUser.find_or_create_by(user: current_user, lesson: @lesson)
+    @lesson_user.update!(completed: true)
+    next_lesson = @course.lessons.where("position > ?", @lesson.position ).order(:position).first
+
+    if next_lesson
+      redirect_to course_lesson_path(@course, next_lesson)
+    else
+      redirect_to course_path(@course), notice: "You have completed the course"
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_lesson
-      @lesson = Lesson.find(params[:id])
-    end
+  
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
+
+  def set_lesson
+    @lesson = Lesson.find(params[:id])
+  end
 end
