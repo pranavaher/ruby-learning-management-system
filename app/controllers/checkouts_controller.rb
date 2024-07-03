f1ff780ebecbc13350e82778b64f17d90581c429
Pranav Aher <doingdifferent29@gmail.com>
2024-07-03 14:17:34 +0530

    Add stripe payment on Unlock Full Course button by adding checkouts route

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,7 +1,26 @@
 class CheckoutsController < ApplicationController
   before_action :authenticate_user!
 
+  stripe_secret_key = Rails.application.credentials.dig(:stripe, :secret_key)
+  Stripe.api_key = stripe_secret_key 
+
   def create
-    
+    course = Course.find(params[:course_id])
+
+    # 4000003560000008
+    session = Stripe::Checkout::Session.create(
+      mode: "payment",
+      line_items: [{
+        price: course.stripe_price_id,
+        quantity: 1,
+      }],
+      success_url: request.base_url + "/courses/#{course.id}",
+      cancel_url: request.base_url + "/courses/#{course.id}",
+      automatic_tax: { enabled: false },
+      customer_email: current_user.email,
+      metadata: { course_id: course.id }
+    )
+
+    redirect_to session.url, allow_other_host: true
   end
 end
