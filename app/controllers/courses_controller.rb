693760e729c104d41e57de02821210db5f0c7d9f
Pranav Aher <doingdifferent29@gmail.com>
2024-07-03 12:35:56 +0530

    Add logic for showing progress bar on course index page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,6 +4,15 @@ class CoursesController < ApplicationController
   # GET /courses or /courses.json
   def index
     @courses = Course.all
+    @user_started_courses = current_user&.lesson_users&.joins(:lesson)&.pluck(:course_id)&.uniq
+    
+    if @user_started_courses.present?
+      @user_course_progresses = @user_started_courses.map do |course_id|
+        course_lessons = Course.find(course_id).lessons.count
+        completed_lessons = current_user&.lesson_users&.joins(:lesson)&.where(completed: true, lesson: { course: course_id })&.count
+        { course_id: course_id, completed_percentage: (completed_lessons.to_f / course_lessons.to_f * 100).to_i }
+      end
+    end
   end
 
   # GET /courses/1 or /courses/1.json
