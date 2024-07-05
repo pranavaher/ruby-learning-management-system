2d5080f18057baaf22c228ecdf84e2e6680314fb
Pranav Aher <doingdifferent29@gmail.com>
2024-07-05 11:07:08 +0530

    Add course create, edit and update action for admin

--- app/controllers/admin/courses_controller.rb
+++ app/controllers/admin/courses_controller.rb
@@ -6,4 +6,34 @@ class Admin::CoursesController < AdminController
   def new
     @admin_course = Course.new
   end
+
+  def create
+    @admin_course = Course.new(admin_course_params)
+
+    if @admin_course.save
+      redirect_to admin_courses_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @admin_course = Course.find(params[:id])    
+  end
+
+  def update
+    @admin_course = Course.find(params[:id])
+
+    if @admin_course.update(admin_course_params)
+      redirect_to admin_courses_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def admin_course_params
+    params.require(:course).permit(:title, :description, :paid, :stripe_price_id, :premium_description, :image)
+  end
 end
