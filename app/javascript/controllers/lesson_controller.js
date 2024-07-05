ddeffecd0d38e52be3efc2d89fde56b36ad4177a
Pranav Aher <doingdifferent29@gmail.com>
2024-07-05 18:01:02 +0530

    Wrote onUpdate function for making request to backend for updating position of lesson

--- app/javascript/controllers/lesson_controller.js
+++ app/javascript/controllers/lesson_controller.js
@@ -1,6 +1,22 @@
-import { Controller } from "@hotwired/stimulus"
+import Sortable from "stimulus-sortable"
 
 // Connects to data-controller="lesson"
-export default class extends Controller {
-  
+export default class extends Sortable {
+  static values = { course: Number }
+
+  onUpdate(event) {
+    super.onUpdate(event)
+    const newIndex = event.newIndex
+    const id = event.item.id
+    const courseId = this.courseValue
+
+    fetch(`/admin/courses/${courseId}/lessons/${id}/move`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRF-Token': document.querySelector('[name="csrf-token"]').content
+      },
+      body: JSON.stringify({ position: newIndex, id: id})
+    })
+  }
 }
