fdde795ef42d8794083b1c08e3b8b7c0ce960c55
Pranav Aher <doingdifferent29@gmail.com>
2024-07-08 15:02:10 +0530

    Fix error while rendering charts

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -9,7 +9,7 @@ class AdminController < ApplicationController
       total_sign_ups: User.count,
     }
 
-    @completed_lesson_by_day = LessonUser.where("created_at > ?", 1.week.ago).where(completed: true).group_by_day(:created_at, format: "%A").count
+    @completed_lessons_by_day = LessonUser.where("created_at > ?", 1.week.ago).where(completed: true).group_by_day(:created_at, format: "%A").count
 
     @sign_ups_by_day = User.where("created_at > ?", 1.week.ago).group_by_day(:created_at, format: "%A").count
 
