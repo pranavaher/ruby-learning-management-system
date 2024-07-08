2638c7162278ec4f4abf6f87c1cb269fc94c5ed1
Pranav Aher <doingdifferent29@gmail.com>
2024-07-08 13:56:44 +0530

    Setup variables for admin dashboard in admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,17 @@ class AdminController < ApplicationController
   before_action :authenticate_admin!
   
   def index
-    @admins = Admin.all
+    @quick_stats = {
+      sign_ups: User.where("created_at > ?", 1.week.ago).count,
+      sales: CourseUser.where("created_at > ?", 1.week.ago).count,
+      completed_lessons: LessonUser.where("created_at > ?", 1.week.ago).where(completed: true).count,
+      total_sign_ups: User.count,
+    }
+
+    @completed_lesson_by_day = LessonUser.where("created_at > ?", 1.week.ago).where(completed: true).group_by_day(:created_at, format: "%A").count
+
+    @sign_ups_by_day = User.where("created_at > ?", 1.week.ago).group_by_day(:created_at, format: "%A").count
+
+    @most_popular_courses = Course.joins(:course_users).group(:id).order("count(course_users.course_id) desc").limit(5)
   end
 end
