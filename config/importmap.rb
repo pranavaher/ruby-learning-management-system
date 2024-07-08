808d0527629e0f9610bdbdea44a0108ae0ed12f6
Pranav Aher <doingdifferent29@gmail.com>
2024-07-08 14:48:16 +0530

    Add charts for completed lessons and sign ups count

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "stimulus-sortable" # @4.1.1
 pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.9
 pin "sortablejs" # @1.15.2
+pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.4.3/dist/chart.js"
+pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js"
